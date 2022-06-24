cb6280ad5702b720b3f3a9cbc1ebac20064b4ed9
amathiyalagan <amathiyalagan@appstec-me.com>
2022-06-24 18:44:46 +0530

    My First Commit

---
+++ PageMethods/SignUpPage.cs
@@ -0,0 +1,90 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ELIT_Selenium_TR.PageMethods
+{
+    class SignUpPage
+    {
+        private IWebDriver driver;
+
+        string signup = "//button[text()='Create Supplier Account']";
+        string mouseaction = "/html/body/div/div/span/div/div[2]/div/div[3]";
+        string accept = "//span[text()='I Agree']";
+        string decline = "//span[text()='I Decline']";
+        string companyname = "//label[text()='Company Name*']//parent::div/div/input"; 
+        string goback = "//span[text()='Go back to Login']";
+
+
+        public SignUpPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void GoToPage(string text)
+        {
+            driver.Navigate().GoToUrl(text);
+        }
+
+        public void Clickon_Signup()
+        {
+            Thread.Sleep(1500);
+            driver.FindElement(By.XPath(signup)).Click();
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+            var element = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.XPath(mouseaction)));
+        }
+
+        public void Mouseaction()
+        {
+            Thread.Sleep(1500);
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+            var element = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.XPath(mouseaction)));
+            Actions action = new Actions(driver);
+            action.MoveToElement(element).Perform();
+            Thread.Sleep(1000);
+        }
+
+        public void Accept()
+        {
+            driver.FindElement(By.XPath(accept)).Click();
+            Thread.Sleep(1000);
+        }
+
+        public Boolean VerifyAccept()
+        {
+            bool res = driver.FindElement(By.XPath(companyname)).Displayed;
+            return res;
+        }
+
+        public void LoginPage()
+        {
+            Thread.Sleep(2000);
+            driver.FindElement(By.XPath(goback)).Click();
+            Thread.Sleep(1000);
+        }
+
+        public void Decline()
+        {
+            driver.FindElement(By.XPath(decline)).Click();
+            Thread.Sleep(1500);
+        }
+
+        public Boolean VerifyDecline()
+        {
+            Thread.Sleep(1000);
+            bool res = driver.FindElement(By.XPath(signup)).Displayed;
+            return res;
+        }
+        
+        public void closeBrowser()
+        {
+            driver.Quit();
+        }
+    }
+}
---
+++ Testcases/SignUpPageTest.cs
@@ -0,0 +1,84 @@
+﻿using AventStack.ExtentReports;
+using ELIT_Selenium_TR.Config;
+using ELIT_Selenium_TR.PageMethods;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ELIT_Selenium_TR.TestCases
+{
+
+   [TestFixture]
+   public class SignUpPageTest:ReportsGenerationClass
+   {
+        SignUpPage signuppage;
+
+        [Test,Order(1)]
+        [Category("User Aggreement")]
+        public void accepting_user_agreement()
+        {
+            signuppage = new SignUpPage(GetDriver());
+            try
+            {
+                signuppage.GoToPage("https://dev.elit.ai/");
+                signuppage.Clickon_Signup();
+                _test.Log(Status.Pass, "Clicked on SignUp");
+
+                signuppage.Mouseaction();
+                signuppage.Accept();
+                _test.Log(Status.Pass, "Accepted Terms and Conditions");
+
+                _test.Log(Status.Pass, "Returned back to LOGIN Page : " + signuppage.VerifyAccept().ToString());
+                Assert.IsTrue(signuppage.VerifyAccept());
+                signuppage.LoginPage();
+            }
+            catch (Exception ex)
+            {
+                DateTime time = DateTime.Now;
+                String fileName = "Screenshot_" + time.ToString("dd_MM_yyyy_hh_mm_ss") + ".png";
+                String screenShotPath = Capture(_driver, fileName);
+                _test.Log(Status.Fail, ex.ToString());
+                _test.Log(Status.Fail, "Snapshot below: " + _test.AddScreenCaptureFromPath("Screenshots\\" + fileName));
+            }
+            finally
+            {
+                signuppage.closeBrowser();
+            }
+        }
+
+        [Test,Order(2)]
+        [Category("User Aggreement")]
+        public void declining_user_agreement()
+        {
+            signuppage = new SignUpPage(GetDriver());
+            try
+            {
+                signuppage.GoToPage("https://dev.elit.ai/");
+                signuppage.Clickon_Signup();
+                _test.Log(Status.Pass, "Clicked on SignUp");
+
+                signuppage.Mouseaction();
+                signuppage.Decline();
+                _test.Log(Status.Pass, "Declined Terms and Conditions");
+
+                _test.Log(Status.Pass,"Returned back to LOGIN Page : " + signuppage.VerifyDecline().ToString());
+                Assert.IsTrue(signuppage.VerifyDecline());
+            }
+            catch (Exception ex)
+            {
+                DateTime time = DateTime.Now;
+                String fileName = "Screenshot_" + time.ToString("dd_MM_yyyy_hh_mm_ss") + ".png";
+                String screenShotPath = Capture(_driver, fileName);
+                _test.Log(Status.Fail, ex.ToString());
+                _test.Log(Status.Fail, "Snapshot below: " + _test.AddScreenCaptureFromPath("Screenshots\\" + fileName));
+            }
+            finally
+            {
+                signuppage.closeBrowser();
+            }
+       }
+    }
+}
